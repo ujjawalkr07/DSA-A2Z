c48b2efd5ece667ce2baff92f6bdad1a4554aaaf
ujjawalkr07 <ujjawalkumar20030102@gmail.com>
2024-07-03 14:18:13 +0530

    pattern

---
+++ pattern.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    // top half
+     for(int i=0;i<n;i++)
+    {
+        // left side
+        for(int j=0;j<i;j++)
+        {
+            cout <<n-j<<" ";
+        }
+        // taking middle element as space in before problem
+        for(int k=0;k<=2*n-3-2*i;k++)
+        {
+            cout <<n-i<<" ";
+        }
+        // right side
+        for(int j=i;j>=0;j--)
+        {
+            cout <<n-j<<" ";
+        }
+        cout<<endl;
+    }
+    // bottom half
+    for(int i=n-2;i>=0;i--)
+    {
+        // left side
+        for(int j=0;j<=i;j++)
+        {
+            cout <<n-j<<" ";
+        }
+        // taking middle element as space in before problem
+        for(int k=0;k<2*n-3-2*i;k++)
+        {
+            cout <<n-i<<" ";
+        }
+        // right side
+        for(int j=i;j>=0;j--)
+        {
+            cout <<n-j<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
