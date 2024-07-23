704b35f3c1ef8af2cc67069e9731d7c2e2207193
UJJAWAL KUMAR <139207332+ujjawalkr07@users.noreply.github.com>
2024-07-24 00:07:06 +0530

    Time: 8 ms (76.81%), Space: 16.9 MB (20.81%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -35,6 +35,8 @@ public:
         */
         
         // better solution
+        /* t.c= O(2*n*m)
+            s.c=O(n)+O(m)
         vector<int>col(m,0);
         vector<int>row(n,0);
         for(int i=0;i<n;i++)
@@ -59,6 +61,60 @@ public:
             }
         }
         
-        
+    */
+    /* optimal approach
+        t.c=O(2*m*n)
+        s.c= O(1)
+    */
+
+    //vector<int>col(m,0);  ->matrix[0][..] 
+    //vector<int>row(n,0);  ->matrix[..][0]
+        int col0=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    // mark the i-th row
+                    matrix[i][0]=0;
+                    // mark the jth col
+                    if(j != 0)
+                    {
+                        matrix[0][j]=0;
+                    }
+                    else
+                    {
+                        col0=0;
+                    }
+                }
+            }
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(matrix[i][j] != 0)
+                {
+                    // check for col and row
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0)
+                    {
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+        if(matrix[0][0] == 0)
+        {
+            for(int j=0;j<m;j++)
+                matrix[0][j]=0;
+        }
+        if(col0 == 0)
+        {
+            for(int i=0;i<n;i++)
+            {
+                matrix[i][0]=0;
+            }
+        }
     }
 };
