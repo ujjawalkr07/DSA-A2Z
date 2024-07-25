f4bb2733c8ed9332ed652810a1f4bf0521ccb62e
UJJAWAL KUMAR <139207332+ujjawalkr07@users.noreply.github.com>
2024-07-25 20:58:29 +0530

    Time: 4 ms (38.57%), Space: 9 MB (12.2%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,6 +3,8 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
+        /* brute force approach
+        t.c=O(m*n), s.c=O(m*n) i.e here we are using extra array to find it
         int arr[m][n];
         for(int i=0;i<m;i++)
         {
@@ -18,5 +20,22 @@ public:
                 matrix[i][j]=arr[i][j];
             }
         }
+        */
+        // optimal approach- 
+        //observation- first taking transpose and the reversing the row of matrix
+         // transpose
+         for(int i=0;i<m-1;i++)
+         {
+            for(int j=i+1;j<n;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+         }
+         //reverse
+         for(int i=0;i<m;i++)
+         {
+            // row is mat[i]
+            reverse(matrix[i].begin(),matrix[i].end());
+         }
     }
 };
