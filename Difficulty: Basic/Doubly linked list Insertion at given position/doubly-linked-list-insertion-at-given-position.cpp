09bfe54708b3fa3c908ce77da320db566b985163
UJJAWAL KUMAR <139207332+ujjawalkr07@users.noreply.github.com>
2024-08-14 14:03:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Doubly linked list Insertion at given position/doubly-linked-list-insertion-at-given-position.cpp
@@ -0,0 +1,130 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* a Node of the doubly linked list */
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x)
+  {
+      data = x;
+      next = prev = NULL;
+  }
+};
+
+void addNode(Node *head, int pos, int data);
+
+Node *insert(Node *head, int x)
+{
+    if (head == NULL)
+    {
+        return new Node(x);
+    }
+    Node *n = new Node(x);
+    
+    head->next = n;
+    n->prev = head;
+    head = n;
+    return head;
+}
+
+void printList(Node *head)
+{
+  // The purpose of below two loops is
+  // to test the created DLL
+  Node *temp=head;
+  if (temp != NULL) {
+    int ct1=0,ct2=0;
+  while (temp->next!=NULL)
+    temp=temp->next,ct1++;
+  while (temp->prev!=NULL)
+   temp = temp->prev,ct2++;
+   if(ct1!=ct2){
+       cout<<-1<<"\n";
+       return;
+   }
+  }
+  while (temp != NULL)
+  {
+      printf("%d ",temp->data);
+      temp=temp->next;
+  }
+  
+  cout << endl;
+}
+
+int main()
+{
+  int t;
+  scanf("%d",&t);
+  while(t--)
+  {
+  Node *head = NULL; 
+  Node *root = NULL;
+  int n;
+  scanf("%d",&n);
+  for(int i=0;i<n;i++){
+     int x;
+     scanf("%d",&x);
+     head = insert(head, x);
+     if(root==NULL) root = head;
+  }     
+  head = root;
+  int pos,data;
+  cin>>pos>>data;
+  addNode(head, pos, data);
+  printList(head);
+  }
+  return 0;
+}
+// } Driver Code Ends
+
+
+/* a Node of the doubly linked list 
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
+//Function to insert a new node at given position in doubly linked list.
+void addNode(Node *head, int pos, int data)
+{
+   // Your code here
+    Node* temp = head;
+    Node* newnode = new Node(data);
+    int count = 0; // Start counting from 1 to represent the position
+
+    // Traverse to the node just before the position
+    while (temp != NULL && count < pos)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    // If the position is beyond the current length, insertion is invalid
+    if (temp == NULL)
+    {
+        // Optional: Handle the error by returning or displaying a message
+        return;
+    }
+
+    // Insert the node after the found position
+    Node* forw = temp->next;
+
+    newnode->next = forw;       // newnode's next is temp's next
+    newnode->prev = temp;       // newnode's prev is temp
+
+    if (forw != NULL)
+    {
+        forw->prev = newnode;   // Update the forward node's previous link
+    }
+
+    temp->next = newnode;       // Update the temp's next link to newnode
+
+}
