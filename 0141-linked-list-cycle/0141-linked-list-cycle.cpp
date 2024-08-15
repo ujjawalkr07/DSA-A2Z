45a9f115053969bb09d4cc20b8145f98d36019d1
UJJAWAL KUMAR <139207332+ujjawalkr07@users.noreply.github.com>
2024-08-15 13:49:48 +0530

    Time: 7 ms (84.55%), Space: 10.9 MB (46.23%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,6 +9,11 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        /* here we are using hash map where we are entering each element 
+        one by one by traversal and if particular element is already present 
+        in map then we will return true or false after traversal of whole loop
+
+        //brute force 
         map<ListNode*,int>mpp;
         ListNode* temp=head;
         while(temp != NULL)
@@ -21,5 +26,20 @@ public:
             temp=temp->next;
         }
         return false;
+        */
+        /* by  using tortoise-Hare method 
+        here we are moving slow by one and fast by two and if fast and slow 
+        comes at same location then we return true*/
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow == fast)
+                return true;
+        }
+        return false;
+        
     }
 };
