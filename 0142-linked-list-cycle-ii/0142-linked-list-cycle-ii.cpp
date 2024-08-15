38357aa3bfd7ff18fc3ec1f90509c590d9c65bb1
UJJAWAL KUMAR <139207332+ujjawalkr07@users.noreply.github.com>
2024-08-15 17:52:52 +0530

    Time: 3 ms (97.68%), Space: 10.4 MB (50.81%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,8 +9,12 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        
+        /*
         // using hash map
+        here if we are putting value in map and check if any element is already
+        present in map then we are returning the temp and if we are not getting 
+        then we are returnnig NULL after traversal of every node
+
         map<ListNode*,int>mpp;
         ListNode* temp=head;
         while( temp != NULL)
@@ -23,6 +27,29 @@ public:
             temp=temp->next;
         }
         return NULL;
+        */
+        /*Tortoise-Hare method
+        step 1:- detecting wheteher it is present or not
+        step 1:- now we are checking for starting point of loop*/
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast != NULL && fast->next != NULL)   // traversing till we reach last element
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow == fast)    // checking if there is loop
+            {
+                slow = head;    // assigning slow to head
+                while(slow != fast)     // traversing till slow and fast become equal
+                {   // we are moving fast and slow by one step
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
+            }
         
+        }
+        return NULL;
+
     }
 };
